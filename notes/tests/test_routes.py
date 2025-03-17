81476d345d6a8178db9b2fcafd752358aaaf7af7
Ekaterina Kolomicheva <keaspb7@yandex.ru>
2025-03-18 00:29:28 +0300

    test

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -1,27 +1,35 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from notes.models import Note
 
+User = get_user_model()
+
+
 class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.notes = Note.objects.create(title='Заголовок', text='Текст')
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.notes = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='Адрес для страницы с заметкой',
+            author=cls.author
+        )
     
     def test_pages_availibility(self):
         urls = (
-            ('',),
-            ('',),
-            ('',),
-            ('',),
-            ('',),
-            ('',),
+            ('notes:home', None),
+            ('notes:detail', ('Адрес для страницы с заметкой',)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
         )
         for name, args in urls:
             with self.subTest(name=name):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
-        
